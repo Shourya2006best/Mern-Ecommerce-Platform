0a1c804972385bb847eecf117c04731aaf9bbd8d
Shourya2006best <thakurshourya2006@gmail.com>
2026-08-22 09:30:43 +0530

    Update Product.jsx

--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/Shopcontext';
-import { assets } from '../assets/assets';
+import { assets } from '../assets/Assets';
 
 import RelatedProducts from '../components/RelatedProducts'; 
 
